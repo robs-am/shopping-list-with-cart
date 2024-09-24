4fdd4ca3c827b0875515af3a47161e498b2f3e1e
Roberta <roberta.amarto@ed3.com.br>
2024-09-23 22:42:13 -0300

    style: update removeButton style

--- src/components/CartWidget/index.tsx
+++ src/components/CartWidget/index.tsx
@@ -18,25 +18,26 @@ const CartWidget: React.FC = () => {
           <div key={product.id} className={classes.cartItem}>
             <span className={classes.cartItemName}>{product.name}</span>
             <div className={classes.cartValuesContainer}>
-            <div className={classes.cartValues}>
-              <span className={classes.cartItemQuantity}>{product.quantity}x</span>
-              <span className={classes.cartItemPriceUnit}>${product.price.toFixed(2)}</span>
-              <span className={classes.cartItemTotal}>${(product.price * product.quantity).toFixed(2)}</span>
-            </div>
-              {/* Botão para remover o produto */}
+              <div className={classes.cartValues}>
+                <span className={classes.cartItemQuantity}>{product.quantity}x</span>
+                <span className={classes.cartItemPriceUnit}>${product.price.toFixed(2)}</span>
+                <span className={classes.cartItemTotal}>${(product.price * product.quantity).toFixed(2)}</span>
+              </div>
+
               <button
                 className={classes.removeButton}
                 onClick={() => removeFromCart(product.id)}
               >
-                X
+               <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
               </button>
-              </div>
-           
+            </div>
+
           </div>
         ))}
 
         <div className={classes.cartTotal}>
-          <strong>Total:</strong> ${totalPrice.toFixed(2)}
+          <span className={classes.orderTotal}>Order Total:</span> 
+          <span>${totalPrice.toFixed(2)}</span>
         </div>
       </div>
     </div>
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -62,14 +62,14 @@ const Product: React.FC = () => {
                       onClick={() => updateCartItemQuantity(product.id, quantity - 1)}
                       className={classes.decrementButton}
                     >
-                      -
+                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
                     </button>
                     <span className={classes.productQuantity}>{quantity}</span>
                     <button
                       onClick={() => updateCartItemQuantity(product.id, quantity + 1)}
                       className={classes.incrementButton}
                     >
-                      +
+                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
                     </button>
                   </>
                 ) : (
