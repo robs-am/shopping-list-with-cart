8ac13e06e07d98cb4f5286caa1767991c1845951
Roberta <roberta.amarto@ed3.com.br>
2024-09-23 23:14:32 -0300

    feat: change state to change border-color

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -16,9 +16,9 @@ interface Product {
 
 const Product: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [addedToCartProductId, setAddedToCartProductId] = useState<number | null>(null); // Estado para controlar o produto adicionado ao carrinho
   const { cart, addToCart, removeFromCart, updateCartItemQuantity } = useCart();
 
-  // Fetch dos dados do JSON
   useEffect(() => {
     fetch('/data/products.json')
       .then((response) => response.json())
@@ -26,10 +26,19 @@ const Product: React.FC = () => {
       .catch((error) => console.error('Erro ao buscar dados:', error));
   }, []);
 
-
   const getProductQuantity = (productId: number) => {
     const productInCart = cart.find((item) => item.id === productId);
     return productInCart ? productInCart.quantity : 0;
+  };
+
+  const handleAddToCart = (product: Product) => {
+    addToCart({
+      id: product.id,
+      name: product.description,
+      price: product.price,
+      quantity: 1,
+    });
+    setAddedToCartProductId(product.id); // Marca o produto como adicionado
   };
 
   return (
@@ -40,7 +49,12 @@ const Product: React.FC = () => {
           const quantity = getProductQuantity(product.id);
 
           return (
-            <div key={product.id} className={classes.productCard}>
+            <div
+              key={product.id}
+              className={`${classes.productCard} ${
+                addedToCartProductId === product.id ? classes.addedToCart : ''
+              }`}
+            >
               <picture>
                 <source media="(min-width: 1024px)" srcSet={product.images.desktop} />
                 <source media="(min-width: 768px)" srcSet={product.images.tablet} />
@@ -52,7 +66,6 @@ const Product: React.FC = () => {
               </picture>
               <p>{product.name}</p>
               <h2>{product.description}</h2>
-              
               <span>${product.price.toFixed(2)}</span>
 
               <div className={classes.quantityControls}>
@@ -62,34 +75,59 @@ const Product: React.FC = () => {
                       onClick={() => updateCartItemQuantity(product.id, quantity - 1)}
                       className={classes.decrementButton}
                     >
-                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="2"
+                        fill="none"
+                        viewBox="0 0 10 2"
+                      >
+                        <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+                      </svg>
                     </button>
                     <span className={classes.productQuantity}>{quantity}</span>
                     <button
                       onClick={() => updateCartItemQuantity(product.id, quantity + 1)}
                       className={classes.incrementButton}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        fill="none"
+                        viewBox="0 0 10 10"
+                      >
+                        <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" />
+                      </svg>
                     </button>
                   </>
                 ) : (
                   <button
-                    onClick={() =>
-                      addToCart({
-                        id: product.id,
-                        name: product.description,
-                        price: product.price,
-                        quantity: 1
-                      })
-                    }
+                    onClick={() => handleAddToCart(product)}
                     className={classes.addToCartButton}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg> Add to Cart
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="21"
+                      height="20"
+                      fill="none"
+                      viewBox="0 0 21 20"
+                    >
+                      <g fill="#C73B0F" clipPath="url(#a)">
+                        <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
+                        <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z" />
+                      </g>
+                      <defs>
+                        <clipPath id="a">
+                          <path fill="#fff" d="M.333 0h20v20h-20z" />
+                        </clipPath>
+                      </defs>
+                    </svg>{' '}
+                    Add to Cart
                   </button>
                 )}
               </div>
             </div>
-
           );
         })}
       </div>
